2c06b25f47c15c8949dc7408dc1f4345d0c5e315
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-08-18 22:39:57 -0400

    comments for DNode object

--- src/structure/DNode.java
+++ src/structure/DNode.java
@@ -3,16 +3,28 @@ package structure;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Node object for HashMap Values
+ * Contains pointer to List of definitions, and pointer to the next HashMap in the "tree"
+ */
 public class DNode {
 
     private ArrayList<Definition> definitions;
     private HashMap<Character, DNode> next;
 
+    /**
+     * Constructor for null DNode
+     */
     public DNode(){
         this.next = new HashMap<>();
         this.definitions = new ArrayList<>();
     }
 
+    /**
+     * Constructor for DNode with Preset values
+     * @param definitions List of Definition Objects
+     * @param next HashMap contains next set of keys and values
+     */
     public DNode(ArrayList<Definition> definitions, HashMap<Character, DNode> next){
         this.next = next;
         this.definitions = definitions;
@@ -24,22 +36,42 @@ public class DNode {
         }
     }
 
+    /**
+     * Getter method for list of definitions
+     * @return List of Definition Objects
+     */
     public ArrayList<Definition> getDefinitions() {
         return definitions;
     }
 
+    /**
+     * Getter method for getting the next hashMap in chain
+     * @return HashMap of next character and DNode pairs
+     */
     public HashMap<Character, DNode> getNext() {
         return next;
     }
 
+    /**
+     * Override and set definitions of Dnode to given List
+     * @param definitions List of Definitions
+     */
     public void setDefinitions(ArrayList<Definition> definitions) {
         this.definitions = definitions;
     }
 
+    /**
+     * Override and set next HashMap to given Map
+     * @param next HashMap of Character, DNode pairs
+     */
     public void setNext(HashMap<Character, DNode> next) {
         this.next = next;
     }
 
+    /**
+     * Override of toString method
+     * @return String representation of DNode object
+     */
     @Override
     public String toString(){
         String message = "";
